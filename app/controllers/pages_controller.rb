df4b851a852488e7921c80b38a9a626928071991
edyta pilorz <edytapilorz@gmail.com>
2023-04-03 17:36:32 +0200

    start page finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home start]
 
   def home
+  end
+
+  def start
+    @categories = ['Meal', 'Apartment', 'Car', 'Mobile and Internet', 'Health', 'Clothing', 'Beauty and hygiene', 'Kids', 'Entertainment and holidays', 'Other', 'Repayment of debts', 'Savings' ]
   end
 end
