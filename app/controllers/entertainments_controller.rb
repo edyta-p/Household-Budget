db20bfebac097bc21b0002f1dd946dfa414f6150
edyta pilorz <edytapilorz@gmail.com>
2023-04-28 16:14:58 +0200

    basic search added to one model

--- app/controllers/entertainments_controller.rb
+++ app/controllers/entertainments_controller.rb
@@ -8,12 +8,21 @@ class EntertainmentsController < ApplicationController
     @entertainment = Entertainment.new(entertainment_params)
     @entertainment.user = current_user
     @entertainment.save!
-    redirect_to root_path
+    redirect_to entertainments_path
   end
+
+  def search
+    @entertainments = Entertainment.all
+    if params[:query].present?
+      @entertainments = @entertainments.where(category: params[:query])
+    end
+  end
+
 
   private
 
   def entertainment_params
     params.require(:entertainment).permit(:category, :date_of_purchase, :amount)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   resources :clothings, only: %i[index create]
   resources :cosmetics, only: %i[index create]
   resources :kids, only: %i[index create]
-  resources :entertainments, only: %i[index create]
+  resources :entertainments, only: %i[index create] do
+    collection do
+      get :search
+    end
+  end
   resources :others, only: %i[index create]
   resources :repayments, only: %i[index create]
   resources :savings, only: %i[index create]
